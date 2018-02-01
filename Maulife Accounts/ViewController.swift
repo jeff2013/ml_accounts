95f28dd442b7849d95289292f8c92e28e607d365
Jeff Chang <jeffrey.chang@twotalltotems.com>
2018-01-31 18:15:21 -0800

    Initial Commit

---
+++ Maulife Accounts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maulife Accounts
+//
+//  Created by Jeff Chang on 2018-01-31.
+//  Copyright © 2018 jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
